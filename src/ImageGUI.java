6e11b9eec19109b12b00c939a0a41a9d685d7314
Marko Lumbar <marko.lumbar@gmail.com>
2025-01-13 17:17:37 +0100

    Added action listener, picture is now shown with the click of a button

--- src/ImageGUI.java
+++ src/ImageGUI.java
@@ -1,13 +1,17 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 //vir: https://www.youtube.com/watch?v=gp9H0WLxKgU&ab_channel=ProgrammingandMathTutorials
-public class ImageGUI {
+public class ImageGUI implements ActionListener {
     private BufferedImage image;
     private JFrame frame;
+    private JPanel centerTextPanel;
 
 
     //vir: https://www.youtube.com/watch?v=PD6pd6AMoOI&list=PLZPZq0r_RZOMhCAyywfnYLlrjiVOkdAI1&index=53&ab_channel=BroCode za border layout
@@ -18,7 +22,7 @@ public class ImageGUI {
         frame.setLayout(new BorderLayout());
         frame.setResizable(true);
 
-        JPanel centerTextPanel = new JPanel();
+        centerTextPanel = new JPanel();
         centerTextPanel.setBackground(Color.RED);
 
         JLabel textlabel = new JLabel("To select an image, click the select button");
@@ -31,6 +35,7 @@ public class ImageGUI {
 
         JButton selectButton = new JButton("Select Image");
         bottomPanel.add(selectButton);
+        selectButton.addActionListener(this);
 
         centerTextPanel.setPreferredSize(new Dimension(700, 100));
         bottomPanel.setPreferredSize(new Dimension(700, 50));
@@ -51,14 +56,37 @@ public class ImageGUI {
                 return;
             }
 
+            centerTextPanel.removeAll();
             JLabel label = new JLabel(new ImageIcon(image));
-            frame.add(label);
-            frame.pack(); //adjust to the size of image
+            centerTextPanel.add(label);
+            //vir: https://javarevisited.blogspot.com/2017/04/difference-between-repaint-and-revalidate-in-Swing-Java.html
+            centerTextPanel.revalidate(); //layout manager recalculates the layout, ko spremenimo component
+            centerTextPanel.repaint(); //redrawing komponente, po spremembi
+
             frame.setVisible(true);
         } catch (Exception e) {
             System.out.println("Error while loading the image: " + e.getMessage());
         }
     }
 
+    //vir: https://docs.oracle.com/javase/8/docs/api/javax/swing/JFileChooser.html za JPG in PNG filter
+    //vir: https://www.youtube.com/watch?v=A6sA9KItwpY&list=PLZPZq0r_RZOMhCAyywfnYLlrjiVOkdAI1&index=66&ab_channel=BroCode za response handling
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("Select an image");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & PNG Images", "jpg", "png");
+        chooser.setFileFilter(filter);
+
+        int response = chooser.showOpenDialog(frame); //vrne integer 0, če file obstaja, obstaja tudi saveDialog
+
+        if (response == JFileChooser.APPROVE_OPTION){ //uspesno izbran file
+            File selectedFile = chooser.getSelectedFile();
+            System.out.println("Choosen file: " + selectedFile.getName());
+            loadImage(selectedFile.getAbsolutePath());
+
+        }
+
+    }
 }
 
